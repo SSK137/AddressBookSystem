74435ba9ceae5b602f437b60c4b1d535900c12d7
SSK137 <saurabhkodam@gmail.com>
2022-04-28 13:51:32 +0530

    AddressBookSystem: Contacts for Address Book

--- src/AddressBookSystem/AddressBookSystem.java
+++ src/AddressBookSystem/AddressBookSystem.java
@@ -3,5 +3,15 @@ package AddressBookSystem;
 public class AddressBookSystem {
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book System");
+
+        Contacts contacts = new Contacts("Saurabh", "Kodam", "Savedi", "Ahmednagar", "Maharashtra","414003", "8149355350","saurabhkodam@gmail.com");
+        System.out.println(contacts.getFirstName());
+        System.out.println(contacts.getLastName());
+        System.out.println(contacts.getEmail());
+        System.out.println(contacts.getAddress());
+        System.out.println(contacts.getCity());
+        System.out.println(contacts.getState());
+        System.out.println(contacts.getContactNo());
+        System.out.println(contacts.getZipCode());
     }
 }
